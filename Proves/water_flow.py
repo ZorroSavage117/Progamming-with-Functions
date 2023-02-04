e519ee43a8a7928ae7a04202210c4cd5883dfeaa
ZorroSavage117 <zorrosavage117@byui.edu>
2023-02-04 13:28:12 -0700

    prove

---
+++ Proves/water_flow.py
@@ -0,0 +1,12 @@
+
+def water_column_height(tower_height, tank_heght):
+    column_height = tower_height + ((3 * tank_heght) / 4)
+    return column_height
+
+def pressure_gain_from_water_height(height):
+    pressure = (998.2 * 9.80665 * height) / 1000
+    return pressure
+
+def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
+    pressure = (-friction_factor * pipe_length * 998.2 * (fluid_velocity ** 2)) / (2000 * pipe_diameter)
+    return pressure
